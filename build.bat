5e2c6cac606ddda85c25b32e5dfc23c4aeb1d723
Vlad <126783011+TheFliss@users.noreply.github.com>
2025-05-20 00:16:41 +0300

    Upload source files

--- build.bat
+++ build.bat
@@ -3,8 +3,6 @@ setlocal
 set build_type=%1
 mkdir cmakebuilds
 cd %~dp0/cmakebuilds
-@REM call "D:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" MinSizeRel
-@REM call "E:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 17" ..
 cmake --build . --config %build_type%
 time /t
